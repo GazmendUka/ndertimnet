88b1061f755806af443a86f677ece78d0db107c5
GazmendUka <132051351+GazmendUka@users.noreply.github.com>
2026-02-06 09:34:12 +0100

    fix: filter active professions only

--- backend/taxonomy/views.py
+++ backend/taxonomy/views.py
@@ -4,14 +4,9 @@ from rest_framework import viewsets, permissions
 from .models import Profession
 from .serializers import ProfessionSerializer
 
-#class ProfessionViewSet(viewsets.ReadOnlyModelViewSet):
-#    queryset = Profession.objects.filter(is_active=True)
-#    serializer_class = ProfessionSerializer
-#    permission_classes = [permissions.AllowAny]
-
-
-# backend/taxonomy/views.py
 class ProfessionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Profession.objects.all()
+    queryset = Profession.objects.filter(is_active=True)
     serializer_class = ProfessionSerializer
     permission_classes = [permissions.AllowAny]
+
+
