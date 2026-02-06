cec91d8ede3d5d2b69af507798de61482d20f306
GazmendUka <132051351+GazmendUka@users.noreply.github.com>
2026-02-06 08:10:19 +0100

    debug: return all professions to verify prod data

--- backend/taxonomy/views.py
+++ backend/taxonomy/views.py
@@ -4,7 +4,14 @@ from rest_framework import viewsets, permissions
 from .models import Profession
 from .serializers import ProfessionSerializer
 
+#class ProfessionViewSet(viewsets.ReadOnlyModelViewSet):
+#    queryset = Profession.objects.filter(is_active=True)
+#    serializer_class = ProfessionSerializer
+#    permission_classes = [permissions.AllowAny]
+
+
+# backend/taxonomy/views.py
 class ProfessionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Profession.objects.filter(is_active=True)
+    queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
     permission_classes = [permissions.AllowAny]
