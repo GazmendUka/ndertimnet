3634c753d681a4db2eba4fe57dcb688369b822f8
GazmendUka <132051351+GazmendUka@users.noreply.github.com>
2026-03-28 15:59:15 +0100

    axios paht fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "http://127.0.0.1:8000/api/";
+  "https://ndertimnet.onrender.com/api/"
 
 
 // ============================================================
