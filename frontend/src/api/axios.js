9200e1270499146a0416f67c5c4d819d275863c6
GazmendUka <132051351+GazmendUka@users.noreply.github.com>
2026-03-29 23:29:01 +0200

    fix av api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://ndertimnet.onrender.com/api/"
+  "https://ndertimnet-r5dt.onrender.com/api/";
 
 
 // ============================================================
